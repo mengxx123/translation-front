d177d313c504c52af65509984318621f3cc2f997
yunser <>
2019-06-24 00:12:40 +0800

    修改布局

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,13 @@
 <template>
-    <my-page title="翻译">
-        <ui-text-field v-model="input" multiLine :rors="6" />
-        <div class="operation">
-            <ui-raised-button class="btn" label="翻译" primary hintText="仅支持英译汉" @click="translate" />
-        </div>
-        <div v-if="result">
-            {{ result.translation[0] }}
+    <my-page title="翻译" :page="page">
+        <div class="common-container container">
+            <ui-text-field v-model="input" multiLine :rors="6" label="英文" />
+            <div class="operation">
+                <ui-raised-button class="btn" label="翻译" primary hintText="仅支持英译汉" @click="translate" />
+            </div>
+            <div v-if="result">
+                {{ result.translation[0] }}
+            </div>
         </div>
     </my-page>
 </template>
@@ -17,13 +19,38 @@
                 input: '',
 //                input: '翻译',
 //                input: 'translate',
-                result: null
+                result: null,
+                page: {
+                    menu: [
+                        // {
+                        //     type: 'icon',
+                        //     icon: 'search',
+                        //     href: 'https://search.yunser.com?utm_source=calculator',
+                        //     target: '_blank',
+                        //     title: '搜索'
+                        // },
+                        {
+                            type: 'icon',
+                            icon: 'apps',
+                            href: 'https://app.yunser.com?utm_source=translation',
+                            target: '_blank',
+                            title: '应用'
+                        }
+                    ]
+                }
             }
         },
         mounted() {
         },
         methods: {
             translate() {
+                if (!this.input) {
+                    this.$message({
+                        type: 'danger',
+                        text: '请输入英文'
+                    })
+                    return
+                }
                 this.$http.get(`/translate?q=${this.input}`).then(
                     response => {
                         let data = response.data
